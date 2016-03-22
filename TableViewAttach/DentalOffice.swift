1c28312e1a8142a7040369bbf182195c00eca641
q45 <quintinsmith@icloud.com>
2016-03-22 16:49:56 -0600

    table view playing around

---
+++ TableViewAttach/DentalOffice.swift
@@ -0,0 +1,11 @@
+//
+//  DentalOffice.swift
+//  TableViewAttach
+//
+//  Created by Quintin Smith on 3/22/16.
+//  Copyright © 2016 wasatchcode. All rights reserved.
+//
+
+import Foundation
+
+
--- TableViewAttach/ViewController.swift
+++ TableViewAttach/ViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
     }
-
-
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell(style: .Default, reuseIdentifier: "Cell")
+        tableView.separatorInset = UIEdgeInsetsZero
+        cell.textLabel?.text = "Sunsets"
+        return cell
+    }
 }
 
